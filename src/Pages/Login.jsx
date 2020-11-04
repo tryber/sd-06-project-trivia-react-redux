c2a52fe6aed34793d62ea29473f0e8c4ab010934
Isadora Koga <isadorakoga710@gmail.com>
2020-11-04 18:21:49 -0300

    :memo: Add API for token

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { login } from '../actions';
+import getToken from '../service/API';
 
 class Login extends React.Component {
   constructor(props) {
     super(props);
     this.checkButton = this.checkButton.bind(this);
+    this.fetchToken = this.fetchToken.bind(this);
     this.state = {
-      buttonDisable: true,
+      buttonDisable: false,
       name: '',
       email: '',
     }
@@ -25,6 +28,16 @@ class Login extends React.Component {
     }
   }
 
+  async fetchToken() {
+    const responseAPI = await getToken();
+    localStorage.setItem('token', responseAPI);
+    const lclStorage = localStorage.getItem('token');
+    // if () {
+
+    // }
+    console.log(lclStorage);
+  }
+
   render() {
     const { buttonDisable, email } = this.state;
     const { getEmail } = this.props;
@@ -38,7 +51,7 @@ class Login extends React.Component {
           Email:
           <input id="email-input" type="text" data-testid="input-gravatar-email" onChange={ this.checkButton } />
         </label>
-        <button type="submit" data-testid="btn-play" disabled={ buttonDisable } onClick={(email) => getEmail(email) }>Jogar</button>
+        <button type="submit" data-testid="btn-play" disabled={ buttonDisable } onClick={ this.fetchToken }>Jogar</button>
       </div>
     )
   };
--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { BrowseRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 
 ReactDOM.render(
-  <BrowseRouter>
-    <App />
-  </BrowseRouter>,
+  <Provider store={ store }>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
