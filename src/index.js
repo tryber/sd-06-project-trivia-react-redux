8e18d1f0da6fbc740333b72b552b898b6baba44f
Thales Rangel <thalesgr@gmail.com>
2020-11-04 19:55:23 -0300

    req 4

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import { Provider } from 'react-redux'
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-// import store from './store';
+import store from './store';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  // <Provider store={ store }>
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  // </Provider>,
+  <Provider store={ store }>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root'),
 );
 
