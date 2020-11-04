09a1cb45c7962ee22e03e6b90f26af6e10566430
Daniel Menescal <danielmjales@bct.ect.ufrn.br>
2020-11-04 18:23:05 -0300

    add settings button, Settings.jsx

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import Login from './components/Login';
 import './App.css';
+import Settings from './components/Settings';
 
 export default function App() {
   return (
-    <div className="App">
-      <Login />
-    </div>
+    <Switch>
+      <Route exact path="/" component={ Login } />
+      <Route path="/settings" component={ Settings } />
+    </Switch>
   );
 }
