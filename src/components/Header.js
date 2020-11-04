046959db7b93fe357038f5a8a7b3ce739a0c3689
isabellavjs <isabellavjs@gmail.com>
2020-11-04 19:16:08 -0300

    Add img and p tags

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,14 @@ import React, { Component } from 'react';
 
 class Header extends Component {
   render() {
+    const { playerName, avatar } = this.props;
     return (
       <div>
-        Header Game
+        <header>
+          <img data-testid="header-profile-picture" src={avatar} alt="Imagem gravatar" />
+          <p data-testid="header-player-name">{playerName}</p>
+          <p data-testid="header-score">0</p>
+        </header>
       </div>
     );
   }
