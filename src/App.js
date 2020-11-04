f9a676cc93bcda02d843705e4803a76dfdbe39c5
Gui-Alucard <guilhermeanv7822@gmail.com>
2020-11-04 18:01:15 -0300

    :sparkles: Login Page
    introduce Login page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Login from './pages/Login';
+
 import logo from './trivia.png';
 import './App.css';
 
@@ -7,9 +9,7 @@ export default function App() {
     <div className="App">
       <header className="App-header">
         <img src={ logo } className="App-logo" alt="logo" />
-        <p>
-          SUA VEZ!
-        </p>
+        <Login />
       </header>
     </div>
   );
