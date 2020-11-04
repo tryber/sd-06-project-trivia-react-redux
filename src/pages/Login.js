52c66c36660bdfb4e75b4bf339d57398a0444daf
Matheuscara <matheuscara2000@gmail.com>
2020-11-04 18:18:10 -0300

    Requirement 0

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,13 +2,51 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class Login extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: '',
+      email: '',
+      isDisabled: true,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.verifyNameAndEmail = this.verifyNameAndEmail.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    }, () => this.verifyNameAndEmail());
+  }
+
+  verifyNameAndEmail() {
+    const { name, email } = this.state;
+    this.setState({ isDisabled: !(name !== '' && email !== '') });
+  }
+
   render() {
+    const { name, email, isDisabled } = this.state;
     return (
       <div>
         FORM
-        <input data-testid="input-player-name" />
-        <input data-testid="input-gravatar-email" />
-        <button type="submit" data-testid="btn-play">Jogar</button>
+        <input
+          value={ name }
+          name="name"
+          onChange={ this.handleChange }
+          data-testid="input-player-name"
+        />
+        <input
+          value={ email }
+          name="email"
+          onChange={ this.handleChange }
+          data-testid="input-gravatar-email"
+        />
+        <button disabled={ isDisabled } type="submit" data-testid="btn-play">
+          Jogar
+        </button>
       </div>
     );
   }
