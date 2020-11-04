802bf77ec3f898a09fe4a29f9210e283f2f65505
VitorSSMoreira <vitorssmoreira@gmail.com>
2020-11-04 18:59:15 -0300

    login page corrections

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import React from 'react';
-import './App.css';
 import Login from './pages/Login';
 
 export default function App() {
   return (
-    <div className="App">
-      <Login />
-    </div>
+    <Login />
   );
 }
