f941700008a20205bd8008cffbb42a0722a10da6
Pedro Duarte <ikbenpedro@gmail.com>
2020-11-04 18:51:46 -0300

    req 4 : header

---
+++ src/pages/Trivia/Components/Header.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+class Header extends Component{
+	constructor(){
+		super();
+
+		state={
+
+		}
+
+	}
+
+	render(){
+		return(
+			<div>
+			<img src={} alt="user-avatar" data-testid="header-profile-picture"/>
+			<span data-testid="header-player-name"/>
+			<span data-testid="header-score"/>
+			</div>
+		)
+	}
+}
