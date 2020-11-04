01268a0ae4ce183f6efff4b7d6b7eb1284a7a82a
VitorSSMoreira <vitorssmoreira@gmail.com>
2020-11-04 20:30:48 -0300

    login page corrections add path pages

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import store from './store';
 
 ReactDOM.render(
   <Provider store={ store }>
---
+++ src/pages/Game.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Game extends React.Component {
+  render() {
+    return GAME;
+  }
+}
+
+export default Game;
