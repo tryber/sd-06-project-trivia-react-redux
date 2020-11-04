079dcc98a4f6d1987609a9887310100542e5a388
isabellavjs <isabellavjs@gmail.com>
2020-11-04 17:05:06 -0300

    Requisito 1

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React, { Component } from 'react';
-import logo from './trivia.png';
+import { Switch, Route } from 'react-router-dom';
+import Login from './pages/Login';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={ logo } className="App-logo" alt="logo" />
-          <p>
-            SUA VEZ
-          </p>
-        </header>
+        <Switch>
+          <Route exact path="/" component={ Login } />
+        </Switch>
       </div>
     );
   }
