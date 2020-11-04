e7ae2d256392a480a400f5a7ff15cc5671d8820a
LeandroParisi <lepcbelisario@gmail.com>
2020-11-04 16:14:58 -0300

    Login inputs and button 2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import logo from './trivia.png';
 import Login from './pages/Login';
 
 export default function App() {
