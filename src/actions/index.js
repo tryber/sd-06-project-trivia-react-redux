50dd39a2206082043aa1534b1567f9f09cce9820
Gui-Alucard <guilhermeanv7822@gmail.com>
2020-11-04 17:13:45 -0300

    :sparkles: Initial Redux Config
    Files and Folders created

---
+++ src/actions/index.js
@@ -0,0 +1,15 @@
+/*
+É um objeto com a propriedade TYPE obrigatória
+e os parametros desejados para serem usadas
+pelos DISPATCHS (mapDispatchToProps)
+Boa pratica é criar uma função que retorna a action(objeto)!
+
+payload é convenção. está email como no Readme.
+*/
+
+export const LOGIN = 'LOGIN';
+
+export const actionLogin = (email) => ({
+  type: LOGIN,
+  email,
+});
---
+++ src/pages/Login.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+// import PropTypes from 'prop-types'
+
+export default class Login extends Component {
+  render() {
+    return (
+      <div>
+        login vem aqui
+      </div>
+    );
+  }
+}
