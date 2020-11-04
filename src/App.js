68fe471d5fd9855f1dd042461e7704ab9a1acf70
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2020-11-04 17:02:44 -0300

    Adicionando setup inicial

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React from 'react';
-import logo from './trivia.png';
-import './App.css';
 
-export default function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>
-          SUA VEZ
-        </p>
-      </header>
-    </div>
-  );
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <span>Hello World !</span>
+      </div>
+    );
+  }
 }
---
+++ src/redux/reducers/index.js
@@ -0,0 +1,15 @@
+// import { combineReducers } from 'redux';
+
+const initialState = {
+  email: '',
+};
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+  default:
+    return state;
+  }
+}
+
+export default reducer;
+// const rootReducer = combineReducers({  });
