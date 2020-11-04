7eb3c4f4bf06b6704a2139492b5f63b1cf2b5dc3
VinicinBraga <vinicius@bragamatta.com.br>
2020-11-04 18:53:54 -0300

    Adicionando Formulario de Login - 1 requisito - Sem lint

---
+++ src/components/LoginForm.js
@@ -0,0 +1,71 @@
+import React from 'react';
+
+class LoginForm extends React.Component {
+	constructor() {
+		super()
+		this.state = {
+			name: '',
+			email: '',
+		}
+		this.emailIsValid = this.emailIsValid.bind(this);
+		this.handleEmailChange = this.handleEmailChange.bind(this);
+		this.handleNameChange = this.handleNameChange.bind(this);
+	}
+
+	emailIsValid(email) {
+		return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)
+	}
+
+	handleNameChange( { target }) {
+		const { value } = target;
+		this.setState({
+			name: value,
+		});
+	}
+
+	handleEmailChange({ target }) {
+		const { value } = target;
+		const email = this.emailIsValid(value);
+		if (email) {
+			this.setState({
+				email: value,
+			})
+		} else {
+			this.setState({
+				email: '',
+			})
+		}
+	}
+
+  render() {
+		const { name, email } = this.state;
+    return(
+			<form>
+				<label htmlFor="name-input">
+					<input
+						data-testid="input-player-name"
+						id="name-input"
+						placeholder="Seu nome"
+						onChange={ this.handleNameChange }
+					/>
+				</label>
+				<label htmlFor="email-input">
+					<input
+						data-testid="input-gravatar-email"
+						id="email-input"
+						placeholder="Seu email"
+						onChange={ this.handleEmailChange }
+					/>
+				</label>
+				<button
+					data-testid="btn-play"
+					disabled={ name && email ? false : true }
+				>
+						Jogar
+				</button>
+			</form>	
+		)
+  }
+}
+
+export default LoginForm;
---
+++ src/pages/Login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import LoginForm from '../components/LoginForm';
+
+export default class Login extends React.Component {
+  render() {
+    return (
+      <nav>
+        <LoginForm />
+      </nav>
+    )
+  }
+}
+
+
