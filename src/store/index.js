336c280535ce1568645353ef629867fb3a15b28d
Pablo Santos <pablo.trybe@gmail.com>
2020-11-04 18:02:04 -0300

    Fixing issues from redux-dev-tools

--- src/store/index.js
+++ src/store/index.js
@@ -2,8 +2,8 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
 
-export default createStore(rootReducer,
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-  ));
+export default createStore(rootReducer, applyMiddleware(thunk));
+// compose(
+//   applyMiddleware(thunk),
+//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+// ));
