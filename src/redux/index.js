a68c604b0b7cc1c4d6b202bab1ee5daf51e8f248
Fábio Senra Corrêa <fabiosenracorrea@hotmail.com>
2020-11-04 17:55:26 -0300

    req1: eslint fix

--- src/redux/index.js
+++ src/redux/index.js
@@ -2,6 +2,6 @@ import { createStore } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, thunk);
 
 export default store;
--- src/redux/reducers/userReducer.js
+++ src/redux/reducers/userReducer.js
@@ -1,12 +1,16 @@
 import { LOGIN } from '../actions';
 import userInitialState from '../states/userState';
 
+function handleLoginStateChange(state, action) {
+  const { email, name, avatar } = action.payload;
+
+  return { ...state, email, name, avatar };
+}
+
 export default function userReducer(state = userInitialState, action) {
   switch (action.type) {
   case LOGIN:
-    const { email, name, avatar } = action.payload;
-
-    return { ...state, email, name, avatar };
+    return handleLoginStateChange(state, action);
   default:
     return state;
   }
