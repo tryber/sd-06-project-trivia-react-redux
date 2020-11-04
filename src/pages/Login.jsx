5dd0243137c5d8e4a97362d557e0191bebb310fd
rodrigoandrademelo <rodrigoandrademelo@gmail.com>
2020-11-04 17:16:13 -0300

    requisito-01-ip

---
+++ src/pages/Login.jsx
@@ -0,0 +1,41 @@
+const { Component } = require("react")
+
+import React, { Component } from 'react';
+
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      email: '',
+      disabled: true,
+    }
+  }
+
+  render() {
+    return (
+      <section>
+        <h1>Login - Play Trivia</h1> 
+        <form>
+          <label htmlFor="input-player-name"> Name
+            <input 
+              data-testid="input-player-name"
+              id ="input-player-name"
+              type="text"
+            />
+          </label>
+          <label htmlFor="input-gravatar-name"> E-Mail
+            <input
+              data-testid="input-gravatar-name"
+              id="input-gravatar-name"
+              type="text"
+            />
+          </label>
+          <button data-testid="btn-play" >
+            Jogar
+          </button>
+        </form>
+      </section>
+    );
+  };
+}
