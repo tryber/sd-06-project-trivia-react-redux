9dc5997bc0d93e7eb157c66b71296d9599eb98b7
Luiz E F Simoes <luizefsimoes@gmail.com>
2020-11-04 17:30:44 -0300

    Criada as rotas

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
-import logo from './trivia.png';
+// import logo from './trivia.png';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={ logo } className="App-logo" alt="logo" />
-        <p>
-          SUA VEZ
-        </p>
-      </header>
+      </header> */}
+      <Switch>
+        <Route exact path="/" component={ Login } />
+        <Route path="/feedback" component={ Feedback } />
+        <Route path="/ranking" component={ Ranking } />
+        <Route path="/jogo" component={ Jogo } />
+        <Route path="/configuracoes" component={ Configuracoes } />
+      </Switch>
     </div>
   );
 }
