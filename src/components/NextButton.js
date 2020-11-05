be6bb83269b7b6abd0be5b871519bca21e6ff828
Aloízio Borges Santos Coelho <aloiziobsc@gmail.com>
2020-11-05 19:31:18 -0300

    Requisito 10

---
+++ src/components/NextButton.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class NextButton extends React.Component {
+  render() {
+    return (
+      <button type="button">
+      Próxima
+      </button>
+    );
+  }
+}
+
+export default NextButton;
