ffb137f7fb3dc3b74750c8b9238e4de9d7941a9e
Bruno Dantas <bruno.wesley77@gmail.com>
2020-11-05 16:40:32 -0300

    Update Feedback

---
+++ src/components/index.js
@@ -0,0 +1,2 @@
+export { default as FeedbackHeader } from './FeedbackHeader';
+export { default as FeedbackMain } from './FeedbackMain';
--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import { FeedbackHeader, FeedbackMain } from '../components';
 
 class Feedback extends Component {
   render() {
     return (
       <div>
+        <FeedbackHeader />
+        <FeedbackMain />
         Feedback
       </div>
     );
