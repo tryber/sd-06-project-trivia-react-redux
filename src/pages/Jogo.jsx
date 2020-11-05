079a9cb144691a40f4daa715fcd935a9febf4051
Isabela Rosa <isabelarosa954@gmail.com>
2020-11-05 18:49:38 -0300

    requisito 4 feito

--- src/pages/Jogo.jsx
+++ src/pages/Jogo.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Header from '../components/Header';
 
 class Jogo extends React.Component {
   render() {
     return (
       <div>
-        Teste
+        <Header />
       </div>
     );
   }
