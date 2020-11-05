106032c0c6ce2cb4b015b00d74909abd783aba7b
Marcelo Pessini <marceloppessini@gmail.com>
2020-11-05 18:04:48 -0300

    requirement04 - ended

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import loginReducer from './login';
 import tokenReducer from './token';
+import gravatarReducer from './gravatar';
 
 const rootReducers = combineReducers({
   loginReducer,
   tokenReducer,
+  gravatarReducer,
 });
 
 export default rootReducers;
