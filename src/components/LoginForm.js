dba79bc83c38e97309da641b1bbfb9f45603243b
Rods <rods.leite27@gmail.com>
2020-11-04 22:46:14 -0300

    juncao do HOME ao LOGIN e implementacao de css

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import Settings from './Settings';
 import fetchApi from '../services';
+import logo from '../trivia.png';
+import configLogo from '../img/config.png';
+import '../App.css';
 
 class LoginForm extends Component {
   render() {
@@ -14,44 +18,52 @@ class LoginForm extends Component {
       showSettings,
     } = this.props;
     return (
-      <form>
-        <div>
-          <label htmlFor="input-player-name">
-            Nome:
-            <input
-              data-testid="input-player-name"
-              id="input-player-name"
-              onChange={ validateName }
-              type="text"
-            />
-          </label>
-          <label htmlFor="input-player-email">
-            Email:
-            <input
-              data-testid="input-gravatar-email"
-              id="input-player-email"
-              onChange={ validateEmail }
-              type="email"
-            />
-          </label>
-        </div>
-        <button
-          type="button"
-          data-testid="btn-play"
-          disabled={ !(validName && validEmail) }
-          onClick={ () => fetchApi() }
-        >
-          Jogar!
-        </button>
-        <button
-          type="button"
-          data-testid="btn-settings"
-          onClick={ () => handleSettings() }
-        >
-          Configurações
-        </button>
-        { showSettings ? <Settings /> : '' }
-      </form>
+      <div className="login-container">
+        <form className="App">
+          <div className="App-header">
+            <img src={ logo } className="App-logo" alt="logo" />
+            <div className="login-div">
+              <label htmlFor="input-player-name">
+                <input
+                  data-testid="input-player-name"
+                  id="input-player-name"
+                  onChange={ validateName }
+                  type="text"
+                  placeholder="Nome"
+                />
+              </label>
+              <label htmlFor="input-player-email">
+                <input
+                  data-testid="input-gravatar-email"
+                  id="input-player-email"
+                  onChange={ validateEmail }
+                  type="email"
+                  placeholder="Email"
+                />
+              </label>
+              <div className="buttons-div">
+                <button
+                  type="button"
+                  data-testid="btn-play"
+                  disabled={ !(validName && validEmail) }
+                  onClick={ () => fetchApi() }
+                >
+                  Jogar!
+                </button>
+                <button
+                  className="button-b"
+                  type="button"
+                  data-testid="btn-settings"
+                  onClick={ () => handleSettings() }
+                >
+                  <img src={ configLogo } width="50" alt="config" />
+                </button>
+                { showSettings ? <Settings /> : '' }
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
     );
   }
 }
