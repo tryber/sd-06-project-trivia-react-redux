ad1bf7430061301ff44dbc1fe92d2f13343fb3dc
Cássio Rubens <cassiorubensadv@gmail.com>
2020-11-05 16:59:09 -0300

    FEAT: Criando Header - Requisito 4

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import GameHeader from '../components/GameHeader';
 
 class Game extends Component {
   render() {
     return (
       <div>
-        Game
+        <GameHeader />
       </div>
     );
   }
