a8c73fef9ed85968799289bfb7227a04b0c20dbe
Bruna Franciele <brunafranciele1902@gmail.com>
2020-11-05 17:30:31 -0300

    atualização dos arquivos

---
+++ src/pages/Game.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Header from '../components/Header';
+import Questions from '../components/Header';
+
+class Game extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Questions />
+      </div>
+    );
+  }
+}
+
+export default Game;
