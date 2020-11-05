626cf5de11d73d58ec9e3a7f05123f46487792c6
Rods <rods.leite27@gmail.com>
2020-11-05 14:12:28 -0300

    arrumando a div config-login-div

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
 import Settings from './Settings';
 import fetchApi from '../services';
 import logo from '../trivia.png';
@@ -22,42 +21,44 @@ class LoginForm extends Component {
         <form className="App">
           <div className="App-header">
             <img src={ logo } className="App-logo" alt="logo" />
-            <div className="login-div">
-              <label htmlFor="input-player-name">
-                <input
-                  data-testid="input-player-name"
-                  id="input-player-name"
-                  onChange={ validateName }
-                  type="text"
-                  placeholder="Nome"
-                />
-              </label>
-              <label htmlFor="input-player-email">
-                <input
-                  data-testid="input-gravatar-email"
-                  id="input-player-email"
-                  onChange={ validateEmail }
-                  type="email"
-                  placeholder="Email"
-                />
-              </label>
-              <div className="buttons-div">
-                <button
-                  type="button"
-                  data-testid="btn-play"
-                  disabled={ !(validName && validEmail) }
-                  onClick={ () => fetchApi() }
-                >
-                  Jogar!
-                </button>
-                <button
-                  className="button-b"
-                  type="button"
-                  data-testid="btn-settings"
-                  onClick={ () => handleSettings() }
-                >
-                  <img src={ configLogo } width="50" alt="config" />
-                </button>
+            <div className="config-login-div">
+              <div className="login-div">
+                <label htmlFor="input-player-name">
+                  <input
+                    data-testid="input-player-name"
+                    id="input-player-name"
+                    onChange={ validateName }
+                    type="text"
+                    placeholder="Nome"
+                  />
+                </label>
+                <label htmlFor="input-player-email">
+                  <input
+                    data-testid="input-gravatar-email"
+                    id="input-player-email"
+                    onChange={ validateEmail }
+                    type="email"
+                    placeholder="Email"
+                  />
+                </label>
+                <div className="buttons-div">
+                  <button
+                    type="button"
+                    data-testid="btn-play"
+                    disabled={ !(validName && validEmail) }
+                    onClick={ () => fetchApi() }
+                  >
+                    Jogar!
+                  </button>
+                  <button
+                    className="button-b"
+                    type="button"
+                    data-testid="btn-settings"
+                    onClick={ () => handleSettings() }
+                  >
+                    <img src={ configLogo } width="50" alt="config" />
+                  </button>
+                </div>
               </div>
               { showSettings ? <Settings /> : '' }
             </div>
