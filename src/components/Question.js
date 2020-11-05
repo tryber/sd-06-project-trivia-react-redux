6ac6edfed84a7d32721a58b0d69ebeadcc8b01a4
rafMewtwo <ponciano.raf@gmail.com>
2020-11-05 20:51:15 -0300

    :rotating_light: style lint

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Question extends React.Component {
-  constructor(){
+  constructor() {
     super();
     this.randomQuestions = this.randomQuestions.bind(this);
     this.shuffleArray = this.shuffleArray.bind(this);
   }
 
   shuffleArray(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+    let currentIndex = array.length, temporaryValue, randomIndex;
     while (0 !== currentIndex) {
       randomIndex = Math.floor(Math.random() * currentIndex);
       currentIndex -= 1;
@@ -25,28 +26,38 @@ class Question extends React.Component {
     const arrayHTML = getQuestions[0].incorrect_answers.map((incorrect, index) => (
     <button key={index} className="btn" data-testid={`wrong-answer`}>{incorrect}</button>
     ));
-    arrayHTML.push(<button key={'correct'} className="btn" data-testid="correct-answer">{getQuestions[0].correct_answer}</button>)
+    arrayHTML.push(<button
+      key={'correct'}
+      className="btn"
+      data-testid="correct-answer"
+    >
+      {getQuestions[0].correct_answer}
+    </button>)
     const newArray = this.shuffleArray(arrayHTML);
     return newArray;
   }
 
   render() {
     const { getQuestions } = this.props;
-    if(getQuestions[0] === undefined ){
+    if (getQuestions[0] === undefined) {
       return <span>'loading...'</span>
     }
     return (
-    <div>
-      <h2 data-testid="question-category">{getQuestions[0].category}</h2>
-      <h1 data-testid="question-text">{getQuestions[0].question}</h1>
-      { this.randomQuestions().map((answer) => answer) }
-    </div>
+      <div>
+        <h2 data-testid="question-category">{getQuestions[0].category}</h2>
+        <h1 data-testid="question-text">{getQuestions[0].question}</h1>
+        { this.randomQuestions().map((answer) => answer) }
+      </div>
     );
   }
+}
+
+Question.propTypes = {
+  getQuestions: PropTypes.arrayOf(Object).isRequired,
 }
 
 const mapStateToProps = (state) => ({
   getQuestions: state.questions.questionsArray,
 });
 
-export default connect(mapStateToProps,)(Question);
+export default connect(mapStateToProps, null)(Question);
