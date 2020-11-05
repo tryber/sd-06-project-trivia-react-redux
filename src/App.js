c70841fe8a0ca2dae31bd4b990746bc685f6f33b
Paulo <paulocheloni@gmail.com>
2020-11-05 15:08:37 -0300

    adding header and feedback

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import Game from './pages/Game';
+import FeedBack from './pages/FeedBack';
 
 export default function App() {
   return (
     <Switch>
       <Route path="game" component={ Game } />
+      <Route path="/feedback" component={ FeedBack } />
       <Route path="/" component={ Login } />
+
     </Switch>
   );
 }
---
+++ src/pages/FeedBack.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import FeedHeader from '../components/FeedHeader';
+
+class FeedBack extends React.Component {
+  render() {
+    return (
+      <FeedHeader />
+    );
+  }
+}
+
+export default FeedBack;
