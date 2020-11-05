ed9d22928ac5ffca2c1ef60eb59f027b0d08a6ed
AlexanderFeitosa <alexander.feitosa@gmail.com>
2020-11-04 21:48:32 -0300

    :bug: Corrigindo Requisito 01

--- src/index.js
+++ src/index.js
@@ -9,11 +9,11 @@ import store from './redux/store';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Provider store={ store }>
+  <Provider store={ store }>
+    <BrowserRouter>
       <App />
-    </Provider>
-  </BrowserRouter>,
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root'),
 );
 
