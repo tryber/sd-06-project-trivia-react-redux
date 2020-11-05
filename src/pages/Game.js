d192b330fc3054e81157e5130d3fbf4e06f68920
Rods <rods.leite27@gmail.com>
2020-11-05 16:44:01 -0300

    funcao map para aperecer na div

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,20 +1,49 @@
 import React from 'react';
+import { fetchQuestions } from '../services';
 
 class Game extends React.Component {
+  constructor() {
+    super();
+    this.getQuestions = this.getQuestions.bind(this);
+    this.state = {
+      questions: [],
+    };
+  }
+
+  async componentDidMount() {
+    const questions = await fetchQuestions(localStorage.getItem('token'));
+    this.getQuestions(questions);
+  }
+
+  getQuestions(questions) {
+    this.setState({
+      questions: [questions],
+    });
+  }
+
   render() {
+    const { questions } = this.state;
     return (
       <div className="game-container">
-        <div className="square">
-          <header>
-            <img
-              data-testid="header-profile-picture"
-              alt="profile"
-            />
-            <p data-testid="header-player-name">Nome da pessoa</p>
-            <p data-testid="header-score">0</p>
-          </header>
-
-        </div>
+        {questions.map((element, index) => {
+          return (
+            <div className="square" key={ element.results[index].question }>
+              <header>
+                <img
+                  data-testid="header-profile-picture"
+                  alt="profile"
+                />
+                <p data-testid="header-player-name">Jogador</p>
+                <p data-testid="header-score">0</p>
+              </header>
+              <div className="questions">
+                <h3 data-testid="question-category">{ element.results[index].category }</h3>
+                <p data-testid="question-text">{ element.results[index].question }</p>
+              </div>
+              <div className="answers">a</div>
+            </div>
+          );
+        })}
       </div>
     );
   }
--- src/services/index.js
+++ src/services/index.js
@@ -4,3 +4,8 @@ export default () => (
     .then((response) => response.token)
     .then((response) => localStorage.setItem('token', response))
 );
+
+export const fetchQuestions = (token) => (
+  fetch(`https://opentdb.com/api.php?amount=5&token=${token}`)
+    .then((response) => response.json())
+);
