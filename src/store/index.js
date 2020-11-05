6a724d25c1ee3887110382fb0ce7d87731e90077
Phelipe Ohlsen <phb.ohlsen@gmail.com>
2020-11-05 20:13:08 -0300

    cypress compose fix?

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,10 @@ import { applyMiddleware, compose, createStore } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
 
+const composeEnhancer = (typeof window !== 'undefined'
+  && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+
 export default createStore(
   rootReducer,
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-  ),
+  composeEnhancer(applyMiddleware(thunk)),
 );
