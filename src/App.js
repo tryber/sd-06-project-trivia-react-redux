ada894c3329c0bf752ff2081c63ad98b2cffe203
Rick Sousa <rickssti@outlook.com>
2020-11-05 14:43:55 -0300

    :sparkles: Adicionado botão de configuração e pagina de configuração

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import Game from './pages/Game';
+import Settings from './pages/Settings';
 
 export default function App() {
   return (
     <Switch>
-      <Route path="game" component={ Game } />
+      <Route path="/settings" component={ Settings } />
+      <Route path="/game" component={ Game } />
       <Route path="/" component={ Login } />
     </Switch>
   );
