c3b821b78b91f68f555793e834059cc2e3809172
William Gomes <williamjog@hotmail.com>
2020-11-05 03:12:01 -0300

    2º requisito - removendo algumas funções de testes desnecessárias

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,18 +1,10 @@
 import React, { Component } from 'react';
-import { getToken } from '../services';
 
 class Game extends Component {
-  handleToken() {
-    const token = getToken();
-    console.log(token);
-  }
-
   render() {
     return (
       <div>
-        <button type="button" onClick={ this.handleToken }>
-          click
-        </button>
+        MA ÔE, GANHOU UM MILHÃO DE REAIS!
       </div>
     );
   }
