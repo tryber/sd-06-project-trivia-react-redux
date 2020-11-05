f0a602b1d87a2cb67f223bb0d440b7a26d0d11fe
Pedro Duarte <ikbenpedro@gmail.com>
2020-11-05 09:39:53 -0300

    req 4- trivia page

--- src/pages/Trivia/Components/Header.jsx
+++ src/pages/Trivia/Components/Header.jsx
@@ -1,22 +1,26 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+// import PropTypes from 'prop-types';
 
 class Header extends Component{
-	constructor(){
-		super();
-
-		state={
-
-		}
-
-	}
-
 	render(){
+		const { avatar, name } = this.props;
 		return(
 			<div>
-			<img src={} alt="user-avatar" data-testid="header-profile-picture"/>
-			<span data-testid="header-player-name"/>
-			<span data-testid="header-score"/>
+			<img src={ avatar } alt="user-avatar" data-testid="header-profile-picture"/>
+			<span data-testid="header-player-name">{ name }</span> <br/>
+			<span data-testid="header-score">0</span>
 			</div>
 		)
 	}
 }
+
+function mapStateToProps(state) {
+  return {
+    avatar: state.user.avatar,
+    name: state.user.name,
+  };
+}
+
+export default connect(mapStateToProps, null)(Header);
+
