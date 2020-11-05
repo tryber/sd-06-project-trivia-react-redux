9d668f218e59d5b0a95b879659b453fb0d6468a5
Flavio Oliveira <flavsoliver@gmail.com>
2020-11-05 16:00:55 -0300

    implementação de botão e tela de config

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import Login from './pages/Login';
+import Settings from './pages/Settings';
 
 class App extends React.Component {
   render() {
     return (
       <Switch>
         <Route exact path="/" component={ Login } />
+        <Route path="/settings" component={ Settings } />
       </Switch>
     );
   }
