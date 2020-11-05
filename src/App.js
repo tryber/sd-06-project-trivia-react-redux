6cfcefc560e6f5b037e41d7f56a295c985828941
Tiago Berwanger <berwangertiago@gmail.com>
2020-11-05 18:23:32 -0300

    Creating all pages

--- src/App.js
+++ src/App.js
@@ -4,11 +4,15 @@ import './App.css';
 import Login from './pages/Login';
 import Gamepage from './pages/Gamepage';
 import Settings from './pages/Settings';
+import Feedback from './pages/Feedback';
+import Ranking from './pages/Ranking';
 
 export default function App() {
   return (
     <div className="App">
       <Switch>
+        <Route exact path="/ranking" component={ Ranking } />
+        <Route exact path="/feedback" component={ Feedback } />
         <Route exact path="/settings" component={ Settings } />
         <Route exact path="/gamepage" component={ Gamepage } />
         <Route exact path="/" component={ Login } />
