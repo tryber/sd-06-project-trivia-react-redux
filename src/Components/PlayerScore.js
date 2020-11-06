5d87dd62f4129a677e5e3546ab8e2e47883d9b86
Cyrano webdev <cyrano@cyranowebdev.com>
2020-11-06 17:03:13 -0300

    req14 - concluindo componente e css.

--- src/Components/PlayerScore.js
+++ src/Components/PlayerScore.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react';
 import { PropTypes } from 'prop-types';
 
+import '../Css/PlayerScore.css';
+
 class PlayerScore extends Component {
   render() {
     const { correctAnswers, score } = this.props;
     return (
-      <section>
+      <section className="results">
         <p>
           Acertos:
-          <span>{ correctAnswers }</span>
+          <span className="correct-answers" data-testid="feedback-total-question">
+            { correctAnswers }
+          </span>
           de 5
         </p>
         <p>
           Score:
-          <span>{ score }</span>
+          <span className="score" data-testid="feedback-total-score">
+            { score }
+          </span>
         </p>
       </section>
     );
