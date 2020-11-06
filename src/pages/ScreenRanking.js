09948dbcc46716acbf8ef324e375d15a45da4b0d
higoramorim <1810042@aluno.univesp.br>
2020-11-06 13:59:22 -0300

    eslint que falta faz

--- src/pages/ScreenRanking.js
+++ src/pages/ScreenRanking.js
@@ -3,27 +3,24 @@ import { Link } from 'react-router-dom';
 import { MD5 } from 'crypto-js/md5';
 
 function ScreenRanking() {
-	
-	const ranking = JSON.parse(localStorage.getItem('RANKING')).sort((a, b
-		) => b.score - a.score);
+  const ranking = JSON.parse(localStorage.getItem('RANKING')).sort((a, b
+	) => b.score - a.score);
 		
-    return(
-			<div className="main-content">
-				{ranking.map((item, index) => { 
-					const hash = MD5(item.picture).toString();
-					return (
-						<div>
-							<span data-test={`player-name-${index}`}>{item.name}</span>
-							<span data-test={`player-name-${index}`}>{item.score}</span>
-								<img src={`https://www.gravatar.com/avatar/${hash}`} alt="image" />
-						</div>);
-})}
+  return(
+    <div className="main-content">
+		{ranking.map((item, index) => { 
+		const hash = MD5(item.picture).toString();
+		return (
+		<div>
+			<span data-test={`player-name-${index}`}>{item.name}</span>
+			<span data-test={`player-name-${index}`}>{item.score}</span>
+		  <img src={`https://www.gravatar.com/avatar/${hash}`} alt="image" />
+    </div>);
+  })}
 		<div>
 			<Link to="/"><button data-testid="btn-go-home">VOLTAR AO INÍCIO</button></Link>
-			
 		</div>
 		</div>)
-	
 }
 
 export default ScreenRanking;
