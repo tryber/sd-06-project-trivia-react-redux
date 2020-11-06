4cdb3ebc8174a852e9cc0a6a0ba3be5df13cc330
Micaeli Teixeira <luime6@gmail.com>
2020-11-05 21:31:29 -0300

    changes

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -15,20 +15,37 @@ class Questions extends Component {
   }
 
   render() {
+    const { question } = this.props;
+    console.log(question);
+
     return (
       <div>
-        <div data-testid="question-category">
-          <p data-testid="question-text"> xxxxxx </p>
-        </div>
-        <div />
+        {question.map((query, index) => (
+          <div data-testid="question-category">
+            <p>
+              {' '}
+              {query.category}
+            </p>
+          </div>
+        ))}
       </div>
+      // <div>
+      //   <div data-testid="question-category">
+      //     <p data-testid="question-text"> xxxxxx </p>
+      //   </div>
+      //   <div />
+      // </div>
     );
   }
 }
 
 const mapStateToProps = (state) => ({
   token: state.user.token,
+  question: state.game.questions,
 });
+
+/* o question do mapStateToProps tem que ser importando ali com question ou outro nome diferente de questions seão da erro,
+alternativa para ficar como questions é fazer toda a requisição do token na pag Game */
 
 const mapDispatchToProps = (dispatch) => ({
   questions: (token) => dispatch(getQuestions(token)),
