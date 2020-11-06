3b1622957f1b4a0e6bdc8e03a2cbdaef035f6461
leandro <leandro.feitoza8018@gmail.com>
2020-11-06 17:56:06 -0300

    Trabalhado o comportamento do botão Proxima

--- src/components/NextButton.js
+++ src/components/NextButton.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class NextButton extends Component {
   render() {
+    const { handleNextQuestion } = this.props;
     return (
       <button
         className="next"
         type="button"
         data-testid="btn-next"
+        onClick={ handleNextQuestion }
       >
         <span>Proxima</span>
       </button>
-    )
+    );
   }
 }
+
+NextButton.propTypes = { handleNextQuestion: PropTypes.func.isRequired };
 
 export default NextButton;
