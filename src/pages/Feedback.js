dddeb533b9fedb2f85551e6fa564da3c2abf8b57
ricardorosa-dev <ricardorosa@gmail.com>
2020-11-06 18:33:25 -0300

    Colocando os elementos manualmente, em vez de um componente Header

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,15 +1,39 @@
 import React from 'react';
-import Header from '../components/header';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class Feedback extends React.Component {
   render() {
+    const { avatar, name, score } = this.props;
     return (
       <div>
-        <Header />
+        <header>
+          <img
+            alt="Player Initials"
+            data-testid="header-profile-picture"
+            src={ avatar }
+          />
+          <h3 data-testid="header-player-name">{ name }</h3>
+          <h4 data-testid="header-score">{ score }</h4>
+        </header>
         <button type="button" data-testid="btn-next">Next</button>
       </div>
     );
   }
 }
 
-export default Feedback;
+function mapStateToProps(state) {
+  return {
+    avatar: state.login.imageProfile,
+    name: state.login.name,
+    score: state.playerData.payload.score,
+  };
+}
+
+export default connect(mapStateToProps)(Feedback);
+
+Feedback.propTypes = {
+  avatar: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  score: PropTypes.number.isRequired,
+};
