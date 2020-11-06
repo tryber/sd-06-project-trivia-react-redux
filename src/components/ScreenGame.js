ff539cd5f633233ecf30da724cbd3683196ddc0b
Pablo Santos <pablo.trybe@gmail.com>
2020-11-06 14:49:52 -0300

    Finishing requisite 7, painting borders when is clicked

--- src/components/ScreenGame.js
+++ src/components/ScreenGame.js
@@ -2,30 +2,26 @@ import React from 'react';
 import { connect } from 'react-redux';
 import propType from 'prop-types';
 import { fetchApiQuestions, requestQuestionsSuccess } from '../actions';
+import '../App.css';
 
 class ScreenGame extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  //   // this.teste = this.teste.bind(this);
-  // }
+  constructor(props) {
+    super(props);
+    this.state = {
+      answered: false,
+    };
+    this.changeColor = this.changeColor.bind(this);
+  }
 
-  componentDidMount() {
-    // const { token } = this.state;
-    // const { getTriviaQuestions, token } = this.props;
-    // console.log(token);
-    // const tokenLocalStorage = localStorage.getItem('token');
-    // getTriviaQuestions(tokenLocalStorage);
-    // console.log(`Imprime token state ${token}`);
-    // console.log(`Imprime token localStorage ${tokenLocalStorage}`);
-    // const resposta = await getTriviaQuestions(tokenLocalStorage);
-    // const { data: { results } } = resposta;
-    // console.log(results);
+  changeColor() {
+    this.setState({
+      answered: true,
+    });
   }
 
   render() {
     const { questions } = this.props;
-    // console.log("Category", questions.results.category);
-    // console.log("Resultado do state questions", questions.tokenReducer.questions[0]);
+    const { answered } = this.state;
     return (
       <div>
         <div className="category">
@@ -50,24 +46,33 @@ class ScreenGame extends React.Component {
         </div>
         <div className="correctAnswer">
           { questions && questions.results && questions.results.map((item) => (
-            <p
+            <button
+              type="button"
               data-testid="correct-answer"
               key={ item.category }
+              id="correct"
+              onClick={ this.changeColor }
+              className={ answered ? 'green-border' : null }
             >
               {item.correct_answer}
-            </p>
+            </button>
           ))}
         </div>
         <div className="incorrectAnswers">
-          {questions && questions.results && questions.results.map((item, index) => (
-            <button
-              type="button"
-              key={ item.question }
-              data-testid={ `wrong-answer-${index}` }
-            >
-              {item.incorrect_answers}
-            </button>
-          ))}
+          {questions
+            && questions.results
+            && questions.results[0].incorrect_answers.map((item, index) => (
+              <button
+                type="button"
+                key={ item.question }
+                data-testid={ `wrong-answer-${index}` }
+                id="incorrect"
+                onClick={ this.changeColor }
+                className={ answered ? 'red-border' : null }
+              >
+                {item}
+              </button>
+            ))}
         </div>
       </div>
     );
