8329cb4f34e5fe36e41f5218674f8ad7d3a72c0b
Pedro Duarte <ikbenpedro@gmail.com>
2020-11-06 16:10:29 -0300

    req18: ranking btn inicio

--- src/pages/Ranking/index.jsx
+++ src/pages/Ranking/index.jsx
@@ -1,11 +1,37 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Ranking extends React.Component {
+  constructor(props) {
+    super(props);
+
+    const ranking = JSON.parse(localStorage.getItem('ranking'));
+
+    this.state = {
+      ranking,
+    };
+  }
+
   render() {
+    const { ranking } = this.state;
     return (
-      <h1 data-testid="ranking-title">
+      <div>
+        <h1 data-testid="ranking-title">
         Ranking
-      </h1>
+        </h1>
+        <ol>
+          { ranking.map((user, index) => (
+            <li key={ `${user.picture}-${index}` }>
+              <div>
+                <img src={ user.picture } alt={ `${user.name}` } />
+                <span data-testid={ ` player-name-${index}` }>{ user.name }</span>
+                <span data-testid={ `player-score-${index}` }>{ user.score }</span>
+              </div>
+            </li>
+          ))}
+        </ol>
+        <Link to="/"> Início </Link>
+      </div>
     );
   }
 }
