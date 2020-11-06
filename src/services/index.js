979cedb12e0bb50a297b2e7192cf06466af60b3e
Matheuscara <matheuscara2000@gmail.com>
2020-11-06 15:50:21 -0300

    teste

--- src/services/index.js
+++ src/services/index.js
@@ -4,9 +4,8 @@ export function fetchAPITrivia() {
     .then((resp) => resp.json());
 }
 
-export function fetchAPIQuestions() {
-  // https://opentdb.com/api.php?amount=${quantidade-de-perguntas-retornadas}&token=${seu-token-aqui}
-  // Recomendação
-  // https://opentdb.com/api.php?amount=5&token=${seu-token-aqui}
-  return '';
+export function fetchAPIQuestions(token) {
+  const endpoint = `https://opentdb.com/api.php?amount=5&token=${token}`;
+  return fetch(endpoint)
+    .then((resp) => resp.json());
 }
