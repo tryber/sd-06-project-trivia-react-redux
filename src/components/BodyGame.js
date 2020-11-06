fb5b4482a427c30a74e71e0c4c8143a8d0ba4a9b
Daniel Jesus <dev.danieljs@gmail.com>
2020-11-05 21:05:39 -0300

    feat: creating the BodyGame component

---
+++ src/components/BodyGame.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchQuestions } from '../actions';
+
+class BodyGame extends Component {
+  componentDidMount() {
+    const { questionsFunction, getToken } = this.props;
+
+    if (getToken !== '') {
+      questionsFunction();
+    }
+  }
+
+  componentDidUpdate(prevProps) {
+    const { getToken, questionsFunction } = this.props;
+
+    if (getToken !== prevProps.getToken && getToken !== '') {
+      console.log(questionsFunction());
+    }
+  }
+
+  render() {
+    const { questions } = this.props;
+    return (
+      <div className="container">
+        {questions.map((question, index) => (
+          <div key={ index }>
+            <div className="box-question">
+              <div className="field-category">
+                <h3 data-testid="question-category">{question.category}</h3>
+              </div>
+              <div className="field-question">
+                <p data-testid="question-text">{question.question}</p>
+              </div>
+            </div>
+            <div className="box-alternatives">
+              <div>
+                <button type="button" data-testid="correct-answer">
+                  {question.correct_answer}
+                </button>
+                {question.incorrect_answers.map(((iten, position) => (
+                  <button
+                    type="button"
+                    key={ position }
+                    data-testid={ `wrong-answer-${position}` }
+                  >
+                    {iten}
+                  </button>
+                )))}
+              </div>
+            </div>
+          </div>
+        )).filter((_, index) => index === 0)}
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  getToken: state.apiReducer.token,
+  questions: state.apiReducer.questions,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  questionsFunction: () => dispatch(fetchQuestions()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(BodyGame);
