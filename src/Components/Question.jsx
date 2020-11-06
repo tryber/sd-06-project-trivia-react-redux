cb50d2a24815af77e061053107da9397355c1f32
Elias Carvalho <eliascarvalho01@hotmail.com>
2020-11-06 17:32:05 -0300

    :memo: - Requeriment 8

--- src/Components/Question.jsx
+++ src/Components/Question.jsx
@@ -10,17 +10,49 @@ class Question extends React.Component {
     this.state = {
       classCorrect: 'btn',
       classIncorrect: 'btn',
-    }
+      question: [],
+      countdown: 30,
+      disabled: false,
+    };
 
     this.randomQuestions = this.randomQuestions.bind(this);
     this.shuffleArray = this.shuffleArray.bind(this);
     this.checkAnswer = this.checkAnswer.bind(this);
+    this.setCountdown = this.setCountdown.bind(this);
+    this.handleRandomQuestion = this.handleRandomQuestion.bind(this);
+  }
+
+  componentDidMount() {
+    this.handleRandomQuestion();
+    setInterval(this.setCountdown, 1000);
+  }
+
+  setCountdown() {
+    const { countdown } = this.state;
+    if (countdown > 0) {
+      this.setState((previous) => ({
+        ...previous,
+        countdown: previous.countdown - 1,
+      }));
+    } else {
+      this.setState({
+        countdown: 0,
+        disabled: true,
+      });
+    }
+  }
+
+  handleRandomQuestion() {
+    const { getQuestions } = this.props;
+    const question = this.randomQuestions(getQuestions);
+    this.setState({ question });
   }
 
   shuffleArray(array) {
-    let currentIndex = array.length,
-      temporaryValue,
-      randomIndex;
+    let currentIndex = array.length;
+    let temporaryValue;
+    let randomIndex;
+
     while (currentIndex !== 0) {
       randomIndex = Math.floor(Math.random() * currentIndex);
       currentIndex -= 1;
@@ -32,25 +64,25 @@ class Question extends React.Component {
   }
 
   checkAnswer() {
-    this.setState(() => {
-      return {
-        classCorrect: 'btn-correct',
-        classIncorrect: 'btn-incorrect'
-      }
-    });
+    this.setState(() => ({
+      classCorrect: 'btn-correct',
+      classIncorrect: 'btn-incorrect',
+    }));
   }
 
   randomQuestions() {
     const { getQuestions } = this.props;
+    const { classIncorrect, classCorrect, disabled } = this.state;
     const arrayHTML = getQuestions[0].incorrect_answers.map((incorrect, index) => {
       return (
         <button
           id="wrong-answer"
           type="button"
-          key={index}
-          className={this.state.classIncorrect}
-          data-testid={`wrong-answer`}
-          onClick={this.checkAnswer}
+          key={ index }
+          disabled={ disabled }
+          className={ classIncorrect }
+          data-testid={ `wrong-answer${index}` }
+          onClick={ this.checkAnswer }
         >
           {incorrect}
         </button>
@@ -61,24 +93,28 @@ class Question extends React.Component {
         id="correct-answer"
         type="button"
         key="correct"
-        className={this.state.classCorrect}
+        disabled={ disabled }
+        className={ classCorrect }
         data-testid="correct-answer"
-        onClick={this.checkAnswer}
+        onClick={ this.checkAnswer }
       >
         {getQuestions[0].correct_answer}
-      </button>
+      </button>,
     );
     const newArray = this.shuffleArray(arrayHTML);
     return newArray;
   }
 
   render() {
+    const { countdown, question } = this.state;
     const { getQuestions } = this.props;
+    console.log(this.state);
     return (
       <div>
         <h2 data-testid="question-category">{getQuestions[0].category}</h2>
         <h1 data-testid="question-text">{getQuestions[0].question}</h1>
-        { this.randomQuestions().map((answer) => answer) }
+        <div>{ question.map((answer) => answer) }</div>
+        <span>{countdown}</span>
       </div>
     );
   }
