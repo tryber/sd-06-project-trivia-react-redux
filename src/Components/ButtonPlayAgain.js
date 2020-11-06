570d8106bab0ff4fd292465678e8a904102cf96d
Cyrano webdev <cyrano@cyranowebdev.com>
2020-11-06 16:52:27 -0300

    refat de elementos html em ButtonPlayAgain

--- src/Components/ButtonPlayAgain.js
+++ src/Components/ButtonPlayAgain.js
@@ -6,13 +6,11 @@ class ButtonPlayAgain extends Component {
   render() {
     const { classProps } = this.props;
     return (
-      <div>
-        <Link to="/">
-          <button data-testid="btn-play-again" type="button" className={ classProps }>
-            Jogar novamente
-          </button>
-        </Link>
-      </div>
+      <Link to="/">
+        <button data-testid="btn-play-again" type="button" className={ classProps }>
+          Jogar novamente
+        </button>
+      </Link>
     );
   }
 }
