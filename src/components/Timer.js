434c71b3725cbaca7b1ceb4c1e28c93043a805ca
Julia Porto <juportob@gmail.com>
2020-11-06 16:26:58 -0200

    G6-R8 - fixing lint

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { renderTime } from '../actions';
 import { connect } from 'react-redux';
+import { renderTime } from '../actions';
 
 class Timer extends Component {
   componentDidMount() {
