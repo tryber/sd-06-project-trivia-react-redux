0a074666ad47adf4c0554907935394a645b8b6f8
Danilo <dansantosalfa@gmail.com>
2020-11-06 21:04:13 -0300

    Requisito 17 work progress

--- src/Pages/Ranking.js
+++ src/Pages/Ranking.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react';
-import RankingList from '../Components/RankingList'
+import { MD5 } from 'crypto-js/md5';
+import ButtonHome from '../Components/ButtonHome';
 
 class Ranking extends Component {
-  
   render() {
+    const ranking = JSON.parse(localStorage.getItem('ranking'));
     return (
       <div>
-        <RankingList />
+        <h1 className="RankingTitle">Ranking</h1>
+        {ranking.sort((a, b) => b.score - a.score)
+          .map((item, index) => {
+            const hash = MD5(item.picture).toString();
+            return (
+              <div key={ index }>
+                <span data-testid={ `player-name-${index}` }>{ item.name }</span>
+                <span data-testid={ `player-score-${index}` }>{ item.score }</span>
+                <img
+                  src={ `https://www.gravatar.com/avatar/ ${hash}` }
+                  alt="gravatar"
+                />
+              </div>
+            );
+          })}
+        <ButtonHome />
       </div>
-    )
+    );
   }
 }
 export default Ranking;
