c87ea9a3ce21f2a33fe549419566eab6b2ff3cd2
Danilo <dansantosalfa@gmail.com>
2020-11-09 16:18:36 -0300

    Test Ranking

--- src/Pages/Ranking.js
+++ src/Pages/Ranking.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
+import { MD5 } from 'crypto-js/md5';
+import ButtonHome from '../Components/ButtonHome';
 
 class Ranking extends Component {
   render() {
+    const ranking = JSON.parse(localStorage.getItem('ranking'));
     return (
       <div>
-        <h1 data-testid="ranking-title">
-          Ranking
-        </h1>
+        <h1 className="RankingTitle" data-testid="ranking-title">Ranking</h1>
+        {ranking.sort((a, b) => b.score - a.score)
+          .map((item, index) => {
+            const hash = MD5(item.picture).toString();
+            return (
+              <div key={ index }>
+                <span data-testid={ `player-name-${index}` }>{ item.name }</span>
+                <span data-testid={ `player-score-${index}` }>{ item.score }</span>
+                <img
+                  src={ `https://www.gravatar.com/avatar/ ${hash}` }
+                  alt="gravatar"
+                />
+              </div>
+            );
+          })}
+        <ButtonHome />
       </div>
     );
   }
