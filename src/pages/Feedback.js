ac379cb4fce9bd8ec834b34b16ecf8bb918e0f1c
Julia Porto <juportob@gmail.com>
2020-11-09 18:21:57 -0200

    G6-R15

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class Feedback extends Component {
   render() {
     return (
       <div>
         <h1 data-testid="feedback-text">Feedback</h1>
+        <Link to='/'data-testid="btn-play-again">
+          Jogar novamente
+        </Link>
       </div>
     );
   }
