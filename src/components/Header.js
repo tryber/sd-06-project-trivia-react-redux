8d3d2163c4ba85ae4f5cd0073d30b0eb10c93dc9
Matheuscara <matheuscara2000@gmail.com>
2020-11-09 18:45:27 -0300

    Requirement 9

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,43 +2,10 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import md5 from 'crypto-js/md5';
 import PropTypes from 'prop-types';
-import { sendScore } from '../actions';
 
 class Header extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      score: 0,
-      timer: 30,
-    };
-  }
-
-  componentDidMount() {
-    const { dispatchScore } = this.props;
-    const { score } = this.state;
-    dispatchScore(score);
-    const mil = 1000;
-    setInterval(() => this.counting(), mil);
-  }
-
-  counting() {
-    const { timer } = this.state;
-    const timeQuestion = 30;
-    if (timer <= timeQuestion && timer > 0) {
-      this.setState((prevState) => ({
-        timer: prevState.timer - 1,
-      }));
-    } else {
-      const wrongAnswer = document.querySelector(
-        '[data-testid="correct-answer"]',
-      );
-      wrongAnswer.disabled = true;
-    }
-  }
-
   render() {
-    const { email, name, score } = this.props;
-    const { timer } = this.state;
+    const { email, name, score, timer } = this.props;
     const hash = md5(email).toString();
     return (
       <div>
@@ -64,10 +31,6 @@ class Header extends Component {
   }
 }
 
-const mapDispatchToProps = (dispatch) => ({
-  dispatchScore: (score) => dispatch(sendScore(score)),
-});
-
 const mapStateToProps = (state) => ({
   email: state.user.email,
   name: state.user.name,
@@ -78,4 +41,4 @@ Header.propTypes = {
   dispatchScore: PropTypes.func,
 }.isRequired;
 
-export default connect(mapStateToProps, mapDispatchToProps)(Header);
+export default connect(mapStateToProps)(Header);
--- src/pages/Game.js
+++ src/pages/Game.js
@@ -9,14 +9,53 @@ class Game extends Component {
     super(props);
     this.state = {
       isLoading: true,
+      timer: 0,
+      interval: {},
     };
     this.updateState = this.updateState.bind(this);
+    this.startingInterval = this.startingInterval.bind(this);
+    this.clearInterval = this.clearInterval.bind(this);
   }
 
   async componentDidMount() {
     const { token, dispatchQuestions } = this.props;
     await dispatchQuestions(token);
     this.updateState();
+    this.startingInterval();
+  }
+
+  startingInterval() {
+    this.setState(
+      {
+        timer: 30,
+      },
+      () => {
+        const mil = 1000;
+        const interval = setInterval(() => this.counting(), mil);
+        this.setState({ interval });
+      },
+    );
+  }
+
+  clearInterval() {
+    const { interval } = this.state;
+    clearInterval(interval);
+  }
+
+  counting() {
+    const { timer } = this.state;
+    const timeQuestion = 30;
+    if (timer <= timeQuestion && timer > 0) {
+      this.setState((prevState) => ({
+        timer: prevState.timer - 1,
+      }));
+    } else {
+      this.clearInterval();
+      const wrongAnswer = document.querySelector(
+        '[data-testid="correct-answer"]',
+      );
+      wrongAnswer.disabled = true;
+    }
   }
 
   updateState() {
@@ -26,11 +65,19 @@ class Game extends Component {
   }
 
   render() {
-    const { isLoading } = this.state;
+    const { isLoading, timer } = this.state;
     return (
       <div>
-        <Header />
-        {isLoading ? <Loading /> : <QuestionCard />}
+        <Header timer={ timer } />
+        {isLoading ? (
+          <Loading />
+        ) : (
+          <QuestionCard
+            timer={ timer }
+            startingInterval={ this.startingInterval }
+            clearIntervalToProps={ this.clearInterval }
+          />
+        )}
       </div>
     );
   }
