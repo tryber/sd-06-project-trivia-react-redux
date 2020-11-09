a66ca3fe2e9bebfbfa604af4d07510da99b3a7c4
SamuelPPlab <samuelpp.s@hotmail.com>
2020-11-09 18:34:15 -0300

    tudo pronto

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -3,29 +3,48 @@ import { Link } from 'react-router-dom';
 import NenhumRegistro from '../components/NenhumRegistroEncontrado';
 import Player from '../components/Player';
 
-let players = JSON.parse(localStorage.ranking);
+class Ranking extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      players: JSON.parse(localStorage.ranking || '[]'),
+    };
+    this.sortingPlayers = this.sortingPlayers.bind(this);
+  }
 
-if (players.length > 1) {
-  players = players.sort((a, b) => (parseInt(b.score, 10) - parseInt(a.score, 10)));
+  componentDidMount() {
+    this.sortingPlayers();
+  }
+
+  sortingPlayers() {
+    const { players } = this.state;
+    if (players.length > 1) {
+      this.setState({ players: players.sort((a, b) => (
+        parseInt(b.score, 10) - parseInt(a.score, 10))) });
+    }
+  }
+
+  render() {
+    const { players } = this.state;
+    return (
+      <div>
+        <h1 data-testid="ranking-title">Melhores Jogadas</h1>
+        <ol>
+          {
+            players ? players.map((entry, index) => (
+              <Player key={ `${index}has` } index={ index } entry={ entry } />
+            ))
+              : <NenhumRegistro />
+          }
+        </ol>
+        <Link to="/">
+          <button data-testid="btn-go-home" type="button">
+            Home
+          </button>
+        </Link>
+      </div>
+    );
+  }
 }
-
-const Ranking = () => (
-  <div>
-    <h1 data-testid="ranking-title">Melhores Jogadas</h1>
-    <ol>
-      {
-        players ? players.map((entry, index) => (
-          <Player key={ `${index}has` } index={ index } entry={ entry } />
-        ))
-          : <NenhumRegistro />
-      }
-    </ol>
-    <Link to="/">
-      <button data-testid="btn-go-home" type="button">
-        Home
-      </button>
-    </Link>
-  </div>
-);
 
 export default Ranking;
