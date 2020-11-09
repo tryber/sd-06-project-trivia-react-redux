326c7f0d841849c0f39de890e7bc52506d514ab7
Marthus Sera <marthus2001@gmail.com>
2020-11-09 17:07:24 -0300

    Ranking Page base code

---
+++ src/pages/Ranking.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Ranking extends React.Component {
+  render() {
+    return (
+      <section>
+        <ol>
+          <li>First Place</li>
+        </ol>
+      </section>
+    );
+  }
+}
+
+export default Ranking;
