7029b6973b812ee52e6680a94882ccc50b12998b
MarioDuarte <marioduarte.engenheiro@gmail.com>
2020-11-09 09:17:46 -0300

    Game finalizado

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Game.css';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import MD5 from 'crypto-js/md5';
 import { Icon } from 'semantic-ui-react';
@@ -12,37 +12,22 @@ class Game extends React.Component {
     super(props);
     this.nextQuestion = this.nextQuestion.bind(this);
     this.decodeHTMLEntities = this.decodeHTMLEntities.bind(this);
+    this.shuffle = this.shuffle.bind(this);
     this.state = {
       stop: false,
       counter: 0,
-      answers: '',
       timer: 30,
+      randomNumber: 0,
     };
   }
 
   async componentDidMount() {
+    this.random();
     const { questionFetch } = this.props;
     const miliseconds = 1000;
     await questionFetch();
-    this.getAnswers();
     this.timerID = setInterval(() => this.timerFunction(), miliseconds);
-  }
-
-  getAnswers() {
-    const { results } = this.props;
-    const { counter } = this.state;
-    const correctAnswer = {
-      correction: 'correct-answer',
-      result: results[counter].correct_answer,
-    };
-    const incorrectAnswers = results[counter].incorrect_answers
-      .map((e, index) => ({
-        correction: `wrong-answer-${index}`,
-        result: e,
-      }));
-    const answers = [correctAnswer, ...incorrectAnswers];
-    this.shuffle(answers);
-    this.setState({ answers });
+    this.random();
   }
 
   timerFunction() {
@@ -69,32 +54,34 @@ class Game extends React.Component {
       counter: counter + 1,
       timer: 30,
     });
-    this.getAnswers();
+    this.random();
+  }
+
+  random() {
+    const four = 4;
+    this.setState({
+      randomNumber: Math.floor(Math.random() * (four - 0)) + 0,
+    });
   }
 
   shuffle(array) {
-    let currentIndex = array.length;
-    let temporaryValue;
-    let randomIndex;
-    while (currentIndex !== 0) {
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex -= 1;
-      temporaryValue = array[currentIndex];
-      array[currentIndex] = array[randomIndex];
-      array[randomIndex] = temporaryValue;
-    }
+    const { randomNumber } = this.state;
+    const index = array.findIndex((e) => e.props['data-testid'] === 'correct-answer');
+    [array[index], array[randomNumber]] = [array[randomNumber], array[index]];
     return array;
   }
 
   render() {
-    const { counter, answers, timer, stop } = this.state;
+    const { counter, timer, stop } = this.state;
     const { name, email, results, score, scoreSum, assertions } = this.props;
     const gravatarLink = 'https://www.gravatar.com/avatar/';
     const emailMD5 = MD5(email);
-    const four = 4;
+    const five = 5;
     localStorage.setItem('state', JSON
       .stringify({ player: { name, score, gravatarEmail: email, assertions } }));
-
+    if (counter === five) {
+      return <Redirect to="/feedback" />;
+    }
     return (
       <div>
         <header className="container-header">
@@ -133,48 +120,44 @@ class Game extends React.Component {
             </div>
           </div>
           <div className="buttons">
-            { answers !== '' ? answers.map((answer, index) => (
-              <button
-                key={ index }
-                style={ answer.correction === 'correct-answer'
-                  ? { border: `${stop ? '3' : '0'}px solid rgb(6, 240, 15)` }
-                  : { border: `${stop ? '3' : '0'}px solid rgb(255, 0, 0)` } }
-                type="button"
-                className="btn btn-secondary btn-lg mt-4 ml-2 mr-2"
-                data-testid={ answer.correction }
-                onClick={ answer.correction === 'correct-answer'
-                  ? () => { scoreSum(timer, counter); this.setState({ stop: true }); }
-                  : () => { this.setState({ stop: true }); } }
-                disabled={ stop }
-              >
-                { this.decodeHTMLEntities(answer.result) }
-              </button>
-            )) : '' }
-            <div className="nextbutton">
-              { counter === four ? (
-                <Link
-                  style={ { textDecoration: 'none' } }
-                  to="/feedback"
-                >
+            { results !== ''
+              ? this
+                .shuffle([...results[counter].incorrect_answers.map((answer, index) => (
                   <button
+                    key={ `incorrect-${index}` }
+                    style={ { border: `${stop ? '3' : '0'}px solid rgb(255, 0, 0)` } }
                     type="button"
-                    className="btn btn-warning btn-block mt-4"
-                    data-testid="btn-next"
-                    style={ stop ? { display: 'block' } : { display: 'none' } }
+                    className="btn btn-secondary btn-lg mt-4 ml-2 mr-2"
+                    data-testid={ `wrong-answer-${index}` }
+                    onClick={ () => { this.setState({ stop: true }); } }
+                    disabled={ stop }
                   >
-                      PRÓXIMA
+                    { this.decodeHTMLEntities(answer) }
                   </button>
-                </Link>)
-                : (
+                )), (
                   <button
+                    key="correct"
+                    style={ { border: `${stop ? '3' : '0'}px solid rgb(6, 240, 15)` } }
                     type="button"
-                    className="btn btn-warning btn-block mt-4"
-                    data-testid="btn-next"
-                    style={ stop ? { display: 'block' } : { display: 'none' } }
-                    onClick={ this.nextQuestion }
+                    className="btn btn-secondary btn-lg mt-4 ml-2 mr-2"
+                    data-testid="correct-answer"
+                    onClick={
+                      () => { scoreSum(timer, counter); this.setState({ stop: true }); }
+                    }
+                    disabled={ stop }
                   >
-                    PRÓXIMA
-                  </button>)}
+                    { this.decodeHTMLEntities(results[counter].correct_answer) }
+                  </button>)]) : '' }
+            <div className="nextbutton">
+              <button
+                type="button"
+                className="btn btn-warning btn-block mt-4"
+                data-testid="btn-next"
+                style={ stop ? { display: 'block' } : { display: 'none' } }
+                onClick={ this.nextQuestion }
+              >
+                PRÓXIMA
+              </button>
             </div>
           </div>
         </div>
