5d8efb77ec843b4ee622f31ac4e305c90e7b8428
MarioDuarte <marioduarte.engenheiro@gmail.com>
2020-11-09 16:49:34 -0300

    Requisito 7, 9 e 11

--- src/reducers/game.js
+++ src/reducers/game.js
@@ -1,18 +1,38 @@
-import { GET_API } from '../actions';
+import { GET_API, SCORE } from '../actions';
 
 const initialState = {
   responseCode: '',
-  results: '',
+  results: [],
+  isFetching: true,
+  gameBoard: {
+    score: 0,
+    assertions: 0,
+  },
 };
 
-export default function user(state = initialState, action) {
+export default function game(state = initialState, action) {
   switch (action.type) {
   case GET_API: {
     const { answer } = action;
     return {
-      ...initialState,
+      ...state,
       responseCode: answer.response_code,
       results: answer.results,
+      isFetching: false,
+    };
+  }
+  case SCORE: {
+    const { timer, counter } = action;
+    const levels = ['easy', 'medium', 'hard'];
+    const { gameBoard: { assertions, score }, results } = state;
+    const ten = 10;
+    return {
+      ...state,
+      gameBoard: {
+        assertions: assertions + 1,
+        score: score + ten
+        + ((levels.findIndex((e) => e === results[counter].difficulty) + 1) * timer),
+      },
     };
   }
   default:
--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,9 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import reducer from '../reducers';
 
-export default createStore(reducer, applyMiddleware(thunk));
+const devTools = typeof window.__REDUX_DEVTOOLS_EXTENSION__ === 'undefined'
+  ? (a) => a : window.__REDUX_DEVTOOLS_EXTENSION__
+  && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+export default createStore(reducer, compose(applyMiddleware(thunk), devTools));
