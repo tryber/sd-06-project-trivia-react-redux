561b21e069ac287190f6a6af8693231b78e82bf1
Flavianny Maciel <flaviannymaciel@gmail.com>
2020-11-09 13:25:14 -0300

    g6-r12e13- alterando a props conforme Req9

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,24 +1,29 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import Header from '../components/Header';
 
 class Feedback extends Component {
   render() {
-    const { awser } = this.props;
-    const tres = 3;
+    const { assertions } = this.props;
+    const three = 3;
     return (
       <div>
         <Header />
         <section data-testid="feedback-text">
-          {awser < tres ? <h3>Podia ser melhor...</h3> : <h3>Mandou bem!</h3>}
+          {assertions < three ? <h3>Podia ser melhor...</h3> : <h3>Mandou bem!</h3>}
         </section>
       </div>
     );
   }
 }
 
+const mapStateToProps = (state) => ({
+  assertions: state.game.assertions,
+});
+
 Feedback.propTypes = {
-  awser: PropTypes.number.isRequired,
+  assertions: PropTypes.number.isRequired,
 };
 
-export default Feedback;
+export default connect(mapStateToProps, null)(Feedback);
