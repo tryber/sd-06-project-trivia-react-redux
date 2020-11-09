858f92dc8a881350eff9e8d6054cfebb11d75f00
Matheus Coutinho <matheuscavalcante88@hotmail.com>
2020-11-09 19:07:08 -0300

    feat: add ranking page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Login from './pages/Login';
 import Questions from './pages/Questions';
 import Settings from './pages/Settings';
 import Feedback from './pages/Feedback';
+import Ranking from './pages/Ranking';
 
 import './App.css';
 
@@ -15,6 +16,7 @@ export default function App() {
         <Route path="/questions" component={ Questions } />
         <Route path="/settings" component={ Settings } />
         <Route path="/feedback" component={ Feedback } />
+        <Route path="/ranking" component={ Ranking } />
       </Switch>
     </div>
   );
