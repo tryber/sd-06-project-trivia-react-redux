eb3c2b137d56067941a0539ad437b5fb4edf59d2
Julia Porto <juportob@gmail.com>
2020-11-09 18:26:43 -0200

    G6-R15 - fixing lint

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 class Feedback extends Component {
@@ -7,7 +6,7 @@ class Feedback extends Component {
     return (
       <div>
         <h1 data-testid="feedback-text">Feedback</h1>
-        <Link to='/'data-testid="btn-play-again">
+        <Link to="/" data-testid="btn-play-again">
           Jogar novamente
         </Link>
       </div>
