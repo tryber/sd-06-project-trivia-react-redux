674765b98bf8439d650a0cbf93677f6122729ddb
Gabriel Luiz da Silva <58389773+gitgabrielluiz@users.noreply.github.com>
2020-11-09 15:11:23 -0300

    requisito 8 - feito

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -8,18 +8,51 @@ import { fetchApi } from '../actions';
 class Game extends React.Component {
   constructor(props) {
     super(props);
+    this.timer = this.timer.bind(this);
     this.firstClick = this.firstClick.bind(this);
     this.nextQuestion = this.nextQuestion.bind(this);
+    this.shuffle = this.shuffle.bind(this);
+    this.getAnswers = this.getAnswers.bind(this);
+    this.decodeHTMLEntities = this.decodeHTMLEntities.bind(this);
     this.state = {
       placar: 0,
       nextButton: 'none',
       counter: 0,
+      answers: '',
+      time: 30,
+      intervalConst: 0,
+      disableAnwsers: false,
     };
   }
 
-  componentDidMount() {
+  async componentDidMount() {
     const { questionFetch } = this.props;
-    questionFetch();
+    await questionFetch();
+    this.getAnswers();
+    this.timer();
+  }
+
+  getAnswers() {
+    const { results } = this.props;
+    const { counter } = this.state;
+    const correctAnswer = {
+      correction: 'correct-answer',
+      result: results[counter].correct_answer,
+    };
+    const incorrectAnswers = results[counter].incorrect_answers
+      .map((e, index) => ({
+        correction: `wrong-answer-${index}`,
+        result: e,
+      }));
+    const answers = [correctAnswer, ...incorrectAnswers];
+    this.shuffle(answers);
+    this.setState({ answers });
+  }
+
+  decodeHTMLEntities(text) {
+    const textArea = document.createElement('textarea');
+    textArea.innerHTML = text;
+    return textArea.value;
   }
 
   firstClick() {
@@ -28,20 +61,63 @@ class Game extends React.Component {
     });
   }
 
-  nextQuestion() {
+  async nextQuestion() {
     const { counter } = this.state;
-    this.setState({
+    await this.setState({
       counter: counter + 1,
+      nextButton: 'none',
     });
+    const { intervalConst } = this.state;
+    clearInterval(intervalConst);
+    this.getAnswers();
+    this.timer();
+  }
+
+  shuffle(array) {
+    let currentIndex = array.length;
+    let temporaryValue;
+    let randomIndex;
+    while (currentIndex !== 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+    return array;
+  }
+
+  // myTimer(time) {
+  //   const atualTime = time - 1;
+  //   console.log(atualTime);
+  //   return atualTime;
+  // }
+
+  timer() {
+    const oneSecond = 1000;
+    const intervalConst = setInterval(() => {
+      const { time } = this.state;
+      console.log(time);
+      this.setState(() => ({
+        time: time - 1,
+      }),
+      () => {
+        if (time <= 1) {
+          console.log('time out');
+          this.setState({
+            disableAnwsers: true,
+          });
+          clearInterval(intervalConst);
+        }
+      });
+    }, oneSecond);
   }
 
   render() {
-    const { placar, nextButton, counter } = this.state;
+    const { placar, nextButton, counter, answers, time, disableAnwsers } = this.state;
     const { name, email, results } = this.props;
     const gravatarLink = 'https://www.gravatar.com/avatar/';
     const emailMD5 = MD5(email);
-
-    console.log(results !== '' ? results[0].category : 0);
 
     return (
       <div>
@@ -51,53 +127,58 @@ class Game extends React.Component {
               data-testid="header-profile-picture"
               alt="imagem"
               src={ gravatarLink + emailMD5 }
+              className="picture"
             />
           </div>
-          <div data-testid="header-player-name">{ name }</div>
+          <div data-testid="header-player-name">
+            Jogador:
+            { name }
+          </div>
           <div data-testid="header-score">
             Placar:
             { placar }
           </div>
         </header>
         <div className="container-game">
-          <div>
+          <div className="right">
             <div data-testid="question-category">
-              { results !== '' ? results[counter].category : 0 }
+              CATEGORIA[
+              { results !== '' ? this.decodeHTMLEntities(results[counter].category) : '' }
+              ]
             </div>
-            <div data-testid="question-text">
-              { results !== '' ? results[counter].question : 0 }
+            <div className="question" data-testid="question-text">
+              { results !== '' ? this.decodeHTMLEntities(results[counter].question) : '' }
+            </div>
+            <div className="timer">
+              Tempo:
+              { time }
             </div>
           </div>
-          <div>
-            <button
-              type="button"
-              className="btn btn-success btn-block mt-4"
-              data-testid="correct-answer"
-              onClick={ this.firstClick }
-            >
-              { results !== '' ? results[counter].correct_answer : 0 }
-            </button>
-            { results !== '' ? results[counter].incorrect_answers.map((answer, index) => (
+          <div className="buttons">
+            { answers !== '' ? answers.map((answer, index) => (
               <button
                 key={ index }
                 type="button"
-                className="btn btn-success btn-block mt-4"
-                data-testid={ `wrong-answer-${index}` }
+                className="btn btn-secondary btn-lg mt-4 ml-2 mr-2"
+                data-testid={ answer.correction }
                 onClick={ this.firstClick }
+                disabled={ disableAnwsers }
               >
-                { answer }
+                { this.decodeHTMLEntities(answer.result) }
               </button>
-            ))
-              : 0 }
-            <button
-              type="button"
-              className="btn btn-success btn-block mt-4"
-              data-testid="btn-next"
-              style={ { display: nextButton } }
-              onClick={ this.nextQuestion }
-            >
-              PRÓXIMA
-            </button>
+            )) : '' }
+            <div className="nextbutton">
+              <button
+                type="button"
+                className="btn btn-warning btn-block mt-4"
+                data-testid="btn-next"
+                style={ { display: nextButton } }
+                onClick={ this.nextQuestion }
+                disabled={ disableAnwsers }
+              >
+                PRÓXIMA
+              </button>
+            </div>
           </div>
         </div>
       </div>
