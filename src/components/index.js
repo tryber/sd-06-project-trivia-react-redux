26e77d52b123311759b92db945ec92d39fc8428c
Lais Gurgel <laisgurgelf@gmail.com>
2020-11-09 19:46:22 -0300

    fixing test

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,3 @@
 export { default as QuestionCard } from './QuestionCard';
 export { default as Header } from './Header';
 export { default as Loading } from './Loading';
-export { default as FeedbackMenssage } from './FeedbackMenssage';
--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class Ranking extends Component {
   render() {
     return (
       <div>
+        <h1 data-testid="ranking-title">Ranking</h1>
         Tela de ranking: Nome e pontuação dos jogadores
+        <button type="button" data-testid="btn-go-home">
+          <Link to="/login">Login</Link>
+        </button>
       </div>
     );
   }
