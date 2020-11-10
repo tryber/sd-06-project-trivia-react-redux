35e21575338f94139824e064f9f277f8bab5bb51
Clayton Miguel Da Silva <claytonmiguel1805@gmail.com>
2020-11-09 23:40:26 -0300

    projeto 100% concluido com problemas de lint

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import TopRanking from './components/TopRanking';
 
 class Ranking extends Component {
   render() {
     const { arrayRanking } = this.props;
     const arrayOrganizado = [...arrayRanking].sort((obj1, obj2) => obj2.score - obj1.score);
+    const arrayOrganizado5Itens = arrayOrganizado.slice(0, 5);
     return (
       <div>
-        <h1>Ranking</h1>
+        <h1 data-testid="ranking-title">Ranking</h1>
         <ul>
-          { arrayOrganizado
+          { arrayOrganizado5Itens
             .map(({
               name,
               picture,
@@ -23,6 +25,14 @@ class Ranking extends Component {
               key={ index }
             />)) }
         </ul>
+        <Link to="/">
+          <button
+            type="button"
+            data-testid="btn-go-home"
+          >
+            Voltar para Início
+          </button>
+        </Link>
       </div>
     );
   }
