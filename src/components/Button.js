f5225137628fbcfa25c6a62fdcb1a34ecce6e441
CristianoNunes <cristianonunes99@gmail.com>
2020-11-10 17:25:54 -0300

    :bug: Requisito 16 - Btn VerRanking-Fixbug linter

--- src/components/Button.js
+++ src/components/Button.js
@@ -5,16 +5,16 @@ export default class Button extends Component {
   render() {
     const { testId, type, name, className, onClick, id, value } = this.props;
     return (
-        <button
-          data-testid={ testId }
-          type={ type }
-          name={ name }
-          className={ className }
-          onClick={ onClick }
-          id={ id }
-        >
-          { value }
-        </button>
+      <button
+        data-testid={ testId }
+        type={ type }
+        name={ name }
+        className={ className }
+        onClick={ onClick }
+        id={ id }
+      >
+        { value }
+      </button>
     );
   }
 }
@@ -22,6 +22,7 @@ export default class Button extends Component {
 Button.defaultProps = {
   type: 'button',
   id: 'button',
+  value: '',
 };
 
 Button.propTypes = {
