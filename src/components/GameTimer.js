cd2de16c9b3ff29fa581a7480836a7dcff169d3d
LeandroParisi <lepcbelisario@gmail.com>
2020-11-10 15:07:50 -0300

    Setting upstream

--- src/components/GameTimer.js
+++ src/components/GameTimer.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { gameTime } from '../actions';
+// teste
 
 class GameTimer extends Component {
   constructor() {
