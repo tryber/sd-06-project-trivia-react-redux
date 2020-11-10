88be12a9621a5455d8a0bb084579c90acbb460b4
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2020-11-10 18:30:38 -0300

    feat: style in feedback

--- src/components/FeedbackMain.js
+++ src/components/FeedbackMain.js
@@ -7,22 +7,48 @@ class Feedback extends Component {
     const { sumCorrectAnswers, score } = this.props;
     const threeHits = 3;
     return (
-      <div>
+      <div className="feedback-main-container">
         {
           sumCorrectAnswers < threeHits
-            ? <h1><span data-testid="feedback-text">Podia ser melhor...</span></h1>
-            : <h1><span data-testid="feedback-text">Mandou bem!</span></h1>
+            ? <h1>
+              <span
+                data-testid="feedback-text"
+                className="feedback-answer"
+              >
+                Podia ser melhor...
+              </span>
+            </h1>
+            : <h1>
+              <span
+                data-testid="feedback-text"
+                className="feedback-answer"
+              >
+                Mandou bem!
+              </span>
+            </h1>
         }
-        <p>
-          Um total de
-          <span data-testid="feedback-total-score">{ score }</span>
-          pontos!
-        </p>
-        <p>
-          Você acertou
-          <span data-testid="feedback-total-question">{ sumCorrectAnswers }</span>
-          questões!
-        </p>
+        <div className="feedback-score">
+          <p className="feedback-text">
+            Um total de
+            <span
+              data-testid="feedback-total-score"
+              className="feedback-total"
+            >
+              { score }
+            </span>
+            pontos!
+          </p>
+          <p className="feedback-text">
+            Você acertou
+            <span
+              data-testid="feedback-total-question"
+              className="feedback-total"
+            >
+              { sumCorrectAnswers }
+            </span>
+            questões!
+          </p>
+        </div>
       </div>
     );
   }
--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
 import { FeedbackHeader, PlayAgain, RankingButton, FeedbackMain } from '../components';
+import './style_sheets/Feedback.scss';
 
 class Feedback extends Component {
   render() {
     return (
-      <div data-testid="feedback-text">
+      <div
+        data-testid="feedback-text"
+        className="feedback-page"
+      >
         <FeedbackHeader />
-        <FeedbackMain />
-        <RankingButton />
-        <PlayAgain />
+        <div className="feedback-container">
+          <FeedbackMain />
+          <div className="feedback-button">
+            <RankingButton />
+            <PlayAgain />
+          </div>
+        </div>
       </div>
     );
   }
