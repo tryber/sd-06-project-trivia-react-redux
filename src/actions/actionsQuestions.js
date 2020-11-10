e461ec717ac72f147d881fee6e6e49267232d43b
Marxneves <marxkhristie@gmail.com>
2020-11-10 18:30:40 -0300

    Settings done

--- src/actions/actionsQuestions.js
+++ src/actions/actionsQuestions.js
@@ -8,6 +8,7 @@ const requestSuccess = (questions) => ({
 const fetchQuestions = (token, settings) => async (dispatch) => {
   const {amount, category, difficult, type} = settings
   const response = await fetch(`https://opentdb.com/api.php?amount=${amount}&category=${category}&difficulty=${difficult}&type=${type}&token=${token}`);
+  console.log(data);
   const data = await response.json();
   dispatch(requestSuccess(data));
 };
