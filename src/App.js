9d1c69f98a8d83aa3315342e033a668a218da460
Flavianny Maciel <flaviannymaciel@gmail.com>
2020-11-10 10:50:59 -0300

    g6-r16

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import Game from './pages/Game';
 import Settings from './pages/Settings';
+import Ranking from './pages/Ranking';
 import Feedback from './pages/Feedback';
 
 export default function App() {
@@ -11,6 +12,7 @@ export default function App() {
       <Route exact path="/" component={ Login } />
       <Route exact path="/game" component={ Game } />
       <Route exact path="/settings" component={ Settings } />
+      <Route exact path="/ranking" component={ Ranking } />
       <Route exact path="/feedback" component={ Feedback } />
     </Switch>
   );
