b82ef7449ff3ce69241bd951bd76fdd75f5c25f9
Matheuscara <matheuscara2000@gmail.com>
2020-11-10 17:46:35 -0300

    tudo pronto meu irmao

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -4,7 +4,7 @@ class Loading extends Component {
   render() {
     return (
       <div>
-        LOADING..
+        LOADING...
       </div>
     );
   }
--- src/pages/Game.js
+++ src/pages/Game.js
@@ -25,15 +25,45 @@ class Game extends Component {
     this.startingInterval();
   }
 
+  shuffle(array) {
+    let currentIndex = array.length;
+    let temporaryValue;
+    let randomIndex;
+
+    // While there remain elements to shuffle...
+    while (currentIndex !== 0) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+  }
+
+  randowChields() {
+    const containerAnswers = document.querySelector('.answers');
+    if (containerAnswers !== null) {
+      const answersAlternatives = [...containerAnswers.children];
+      this.shuffle(answersAlternatives);
+      answersAlternatives.map((element) => containerAnswers.appendChild(element));
+    }
+  }
+
   startingInterval() {
     this.setState(
       {
-        timer: 30,
+        timer: 5,
       },
       () => {
         const mil = 1000;
         const interval = setInterval(() => this.counting(), mil);
         this.setState({ interval });
+        this.randowChields();
       },
     );
   }
@@ -52,10 +82,13 @@ class Game extends Component {
       }));
     } else {
       this.clearInterval();
-      const wrongAnswer = document.querySelector(
+      const correctAnswer = document.querySelector(
         '[data-testid="correct-answer"]',
       );
-      wrongAnswer.disabled = true;
+      correctAnswer.disabled = true;
+      document.querySelector(
+        '[data-testid="wrong-answer-0"]',
+      ).click();
     }
   }
 
