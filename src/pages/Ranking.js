e2d2c6c62b6ebeb79517fed36ddaaeeab9964a15
Marthus Sera <marthus2001@gmail.com>
2020-11-10 16:35:18 -0300

    Fix array sort

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -3,7 +3,13 @@ import React from 'react';
 class Ranking extends React.Component {
   render() {
     const ranking = localStorage.getItem('ranking').split(',');
-    ranking.sort(() => ranking.score);
+    ranking.sort((a, b) => {
+      const scoreA = a.score;
+      const scoreB = b.score;
+      const minusOne = -1;
+      if (scoreA > scoreB) return 1;
+      if (scoreA < scoreB) return minusOne;
+    });
 
     return (
       <section>
