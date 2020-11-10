33e3fe87db66b227f9fc253d4e432664d301bfbf
Renan Cunha <renan2610@gmail.com>
2020-11-10 16:27:46 -0300

    Retidando .only do teste

--- cypress/integration/project.spec.js
+++ cypress/integration/project.spec.js
@@ -566,7 +566,7 @@ describe('17 - [TELA DE RANKING] Crie a tela de _ranking_', () => {
     });
   });
 
-  it.only('Devem existir duas pessoas no _ranking_', () => {
+  it('Devem existir duas pessoas no _ranking_', () => {
     cy.get(BUTTON_GO_HOME_SELECTOR).click();
     cy.get(INPUT_PLAYER_NAME_SELECTOR).clear();
     cy.get(INPUT_PLAYER_EMAIL_SELECTOR).clear();
