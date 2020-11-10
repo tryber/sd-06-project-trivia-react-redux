563ff0a00f8e375d13fa4ad279b59138b92889e1
FlavioSugano <flavio.sugano@gmail.com>
2020-11-10 18:29:47 -0300

    Tela de ranking: criaçao do CSS da pagina

--- src/Pages/Ranking.js
+++ src/Pages/Ranking.js
@@ -1,24 +1,55 @@
 import React, { Component } from 'react';
 import ButtonHome from '../Components/ButtonHome';
 
+import '../Css/Ranking.css';
+
 class Ranking extends Component {
   render() {
     const ranking = JSON.parse(localStorage.getItem('ranking'));
     return (
-      <div>
-        <h1 className="RankingTitle" data-testid="ranking-title">Ranking</h1>
-        {ranking.sort((a, b) => parseInt(b.score, 10) - parseInt(a.score, 10))
-          .map((item, index) => (
-            <div key={ index }>
-              <span data-testid={ `player-name-${index}` }>{ item.name }</span>
-              <span data-testid={ `player-score-${index}` }>{ item.score }</span>
-              <img
-                src={ item.picture }
-                alt="gravatar"
-              />
-            </div>
-          ))}
-        <ButtonHome />
+      <div className="Ranking-page">
+        <ButtonHome className="Button-home" />
+        <div className="Ranking-board">
+          <h1 className="Ranking-title" data-testid="ranking-title">
+            <span
+              role="img"
+              aria-label="Trophy"
+              className="Emoji-trophy"
+            >
+              &#127942;
+            </span>
+            Ranking
+            <span
+              role="img"
+              aria-label="Trophy"
+              className="Emoji-trophy"
+            >
+              &#127942;
+            </span>
+          </h1>
+          {ranking.sort((a, b) => parseInt(b.score, 10) - parseInt(a.score, 10))
+            .map((item, index) => (
+              <div key={ index } className="Player-record">
+                <img
+                  src={ item.picture }
+                  className="Profile-picture"
+                  alt="gravatar"
+                />
+                <span
+                  data-testid={ `player-name-${index}` }
+                  className="Player-name"
+                >
+                  { item.name }
+                </span>
+                <span
+                  data-testid={ `player-score-${index}` }
+                  className="Player-score"
+                >
+                  { item.score }
+                </span>
+              </div>
+            ))}
+        </div>
       </div>
     );
   }
