e6b57f093809e56452225576c5703b1943008f5b
LeandroParisi <lepcbelisario@gmail.com>
2020-11-10 15:08:40 -0300

    Requisito 17 finalizado: Score do redux setado para 0 no willMount do Login

--- src/components/GameTimer.js
+++ src/components/GameTimer.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { gameTime } from '../actions';
-// teste
 
 class GameTimer extends Component {
   constructor() {
