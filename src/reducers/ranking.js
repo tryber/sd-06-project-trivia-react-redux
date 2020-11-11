bf7a5522e82e4307a74f7a21d9820b34ba318ddd
Renan Cunha <renan2610@gmail.com>
2020-11-10 22:45:07 -0300

    Ajustando o ranking do localStorage para salvar mais de um jogador e ser carregado no reducer

--- src/reducers/ranking.js
+++ src/reducers/ranking.js
@@ -1,12 +1,14 @@
-import { SAVE_RANKING } from '../actions/index';
+import { SAVE_RANKING, LOAD_RANKING } from '../actions/index';
 
 const initialState = [];
 
 function ranking(state = initialState,
-  { type, playerAtual }) {
+  { type, playerAtual, localStoreRanking }) {
   switch (type) {
   case SAVE_RANKING:
     return [...state, playerAtual];
+  case LOAD_RANKING:
+    return [...localStoreRanking];
   default:
     return state;
   }
