5e1c4b9153dd4685ecfb0d9fbe846aafb5079841
Tiago Berwanger <berwangertiago@gmail.com>
2020-11-11 13:46:27 -0300

    refact

--- src/pages/Gamepage.js
+++ src/pages/Gamepage.js
@@ -2,8 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import md5 from 'crypto-js/md5';
-import Questions from '../components/Questions';
-// import CountdownTimer from '../components/CountdownTimer';
+import questionsAPI from '../services/questionAPI';
 
 class Gamepage extends React.Component {
   constructor() {
@@ -11,18 +10,27 @@ class Gamepage extends React.Component {
     this.state = {
       questionIndex: 0,
       disableButton: false,
+      buttonBorder: false,
+      timer30: 30,
     };
     this.changeQuestion = this.changeQuestion.bind(this);
     this.changePage = this.changePage.bind(this);
+    // _________________________________________
+    this.handleClick = this.handleClick.bind(this);
+    this.questionsGet = this.questionsGet.bind(this);
+    this.countdown = this.countdown.bind(this);
   }
 
   componentDidMount() {
-    const timingDisable = 3000;
-    setTimeout(() => {
-      this.setState({
-        disableButton: true,
-      });
-    }, timingDisable);
+    this.questionsGet();
+    const miliseconds = 1000;
+    this.aux = setInterval(this.countdown, miliseconds);
+    this.countdown(this.aux);
+    console.log('test');
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.aux);
   }
 
   changePage() {
@@ -37,11 +45,49 @@ class Gamepage extends React.Component {
     const number = 4;
     if (questionIndex === number) {
       this.changePage();
-    } else {
       this.setState((state) => (
-        { questionIndex: (state.questionIndex + 1) % questions.length }
+        {
+          questionIndex: (state.questionIndex + 1) % questions.length,
+          disableButton: false,
+          buttonBorder: false,
+          timer30: 30,
+        }
       ));
     }
+    const miliseconds = 1000;
+    this.aux = setInterval(this.countdown, miliseconds);
+    this.countdown(this.aux);
+  }
+
+  // _________________________________________________
+
+  async questionsGet() {
+    const tokenLocal = localStorage.getItem('token');
+    await questionsAPI(tokenLocal);
+  }
+
+  countdown() {
+    const { timer30 } = this.state;
+    // if (timer30 >= 0) {
+    // clearInterval(this.aux);
+    this.setState({
+      timer30: timer30 - 1,
+    });
+    // } else {
+    //   this.handleClick();
+    // }
+    if (timer30 <= 0) {
+      clearInterval(this.aux);
+      this.handleClick()
+    }
+    
+  }
+
+  handleClick() {
+    const { buttonBorder } = this.state;
+    this.setState({
+      buttonBorder: !buttonBorder,
+    });
   }
 
   render() {
@@ -51,9 +97,16 @@ class Gamepage extends React.Component {
     // console.log('index render:', questionIndex);
     const questionAtual = questions[questionIndex];
     const hash = md5(email);
+    // ______________________________________________
+    const { buttonBorder } = this.state;
+    // const { questionAtual } = this.props;
+    const { timer30 } = this.state;
     return (
       <div className="gamepage-container">
         <header className="gamepage-header">
+          <div>
+            {timer30}
+          </div>
           <img
             src={ `https://www.gravatar.com/avatar/${hash}` }
             alt="gravatar"
@@ -70,17 +123,60 @@ class Gamepage extends React.Component {
           >
             Placar: 0
           </span>
+          <br />
         </header>
-        <Questions questionAtual={ questionAtual } />
-        { disableButton && (
+        <div className="gamepage-questions">
+          <div
+            data-testid="question-category"
+            className="question-category"
+          >
+            Categoria:
+            {questionAtual && questionAtual.category}
+            <br />
+          </div>
+          <div
+            data-testid="question-text"
+            className="question-text"
+          >
+            Pergunta:
+            <br />
+            {questionAtual && questionAtual.question}
+          </div>
+        </div>
+        <div className="gamepage-answer">
+          {questionAtual && questionAtual.incorrect_answers
+            .map((result, i) => (
+              <div key={ result }>
+                <button
+                  className={ !buttonBorder ? 'none-answer' : 'wrong' }
+                  onClick={ this.handleClick }
+                  data-testid={ `wrong-answer-${i}` }
+                  type="button"
+                  disabled={ buttonBorder }
+                >
+                  {result}
+                </button>
+              </div>
+            ))}
           <button
-            data-testId="btn-next"
+            className={ !buttonBorder ? 'none-answer' : 'correct' }
+            onClick={ this.handleClick }
+            data-testid="correct-answer"
+            type="button"
+            disabled={ buttonBorder }
+          >
+            {questionAtual && questionAtual.correct_answer}
+          </button>
+          {/* { disableButton && ( */}
+          <button
+            data-testid="btn-next"
             type="button"
             onClick={ () => this.changeQuestion() }
           >
             PRÓXIMA
           </button>
-        )}
+          {/* )} */}
+        </div>
       </div>
     );
   }
@@ -95,6 +191,7 @@ const mapStateToProps = (state) => ({
 export default connect(mapStateToProps)(Gamepage);
 
 Gamepage.propTypes = {
+  questionAtual: PropTypes.arrayOf(Object).isRequired,
   email: PropTypes.string.isRequired,
   username: PropTypes.string.isRequired,
   questions: PropTypes.object.isRequired,
