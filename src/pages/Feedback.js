bc8a8f88408670db9e21b046935481bf811d5175
Victor-Mateus-Ferreira <victormateuslf@gmail.com>
2020-11-12 09:48:35 -0300

    player data in feedback page

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import md5 from 'crypto-js/md5';
 import { Link } from 'react-router-dom';
 
 class Feedback extends React.Component {
   render() {
+    const { email, username } = this.props;
+    const hash = md5(email);
     return (
       <div>
+        <header>
+          <img
+            src={ `https://www.gravatar.com/avatar/${hash}` }
+            alt="gravatar"
+            data-testid="header-profile-picture"
+          />
+          <p
+            data-testid="header-player-name"
+          >
+            {username}
+          </p>
+          <span
+            data-testid="header-score"
+          >
+            Placar: 0
+          </span>
+        </header>
         <p
           data-testid="feedback-text"
         >
@@ -31,4 +53,14 @@ class Feedback extends React.Component {
   }
 }
 
-export default Feedback;
+const mapStateToProps = (state) => ({
+  email: state.login.email,
+  username: state.login.username,
+});
+
+Feedback.propTypes = {
+  email: PropTypes.string.isRequired,
+  username: PropTypes.string.isRequired,
+}.isRequired;
+
+export default connect(mapStateToProps)(Feedback);
