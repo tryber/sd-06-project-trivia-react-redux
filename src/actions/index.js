34c9fd9b81cc3b63321f6425e1a81ca6d562e3c1
Zeca Costa <jccostaso@gmail.com>
2020-11-15 14:28:45 -0300

    some requirements: score and points

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,6 +3,7 @@ import questionsAPI from '../services/questionAPI';
 export const LOGIN = 'LOGIN';
 export const QUESTION = 'QUESTION';
 export const SCORE = 'SCORE';
+export const ASSERTIONS = 'ASSERTIONS';
 
 export const loginAction = (email, username) => ({
   type: LOGIN,
@@ -20,18 +21,35 @@ export const scoreAction = (score) => ({
   score,
 });
 
+export const assertionsAction = (assertions) => ({
+  type: ASSERTIONS,
+  assertions,
+});
+
 export const questionsThunk = () => async (dispatch) => {
   const tokenLocal = localStorage.getItem('token');
   const questionsReturn = await questionsAPI(tokenLocal);
   dispatch(questionsAction(questionsReturn));
 };
 
-export function updateScore(score) {
+// export function updateScore(score) {
+//   return (dispatch, getState) => {
+//     const { name, email } = getState().login;
+//     dispatch(scoreAction(score));
+//     const StoragedPlayer = { player:
+//       { name, gravatarEmail: email, score, assertions: 0 },
+//     };
+//     localStorage.setItem('state', JSON.stringify(StoragedPlayer));
+//   };
+// }
+
+export function ScoreAndAssertionsFuncion(score, assertions) {
   return (dispatch, getState) => {
     const { name, email } = getState().login;
     dispatch(scoreAction(score));
+    dispatch(assertionsAction(assertions));
     const StoragedPlayer = { player:
-      { name, gravatarEmail: email, score, assertions: 0 },
+    	{ name, gravatarEmail: email, score, assertions }
     };
     localStorage.setItem('state', JSON.stringify(StoragedPlayer));
   };
