eb7172f2905759923704e793bc43c374ddd5bf56
Clayton Miguel Da Silva <claytonmiguel1805@gmail.com>
2020-11-27 15:40:40 -0300

    indo-alem-dos-requisitos-clayton-trivia

--- src/pages/Configuration.js
+++ src/pages/Configuration.js
@@ -1,12 +1,44 @@
 import React, { Component } from 'react';
+import { apiCategory } from '../services/request';
 
 class Configuration extends Component {
+  constructor() {
+    super();
+    this.state = {
+      categories: [],
+    };
+  }
+
+  async categories() {
+    const categories1 = await apiCategory();
+    return categories1;
+  }
+
   render() {
     return (
       <div>
         <h1 data-testid="settings-title">
           Configuration
         </h1>
+        <label>
+          Categoria:
+          <select>
+            {categories2
+              .map(({ id, name }) => <option key={ id }value={ id }>{ name }</option>)}
+          </select>
+        </label>
+        <label>
+          Dificuldade:
+          <select>
+            <option></option>
+          </select>
+        </label>
+        <label>
+          Tipo:
+          <select>
+            <option></option>
+          </select>
+        </label>
       </div>
     );
   }
--- src/services/request.js
+++ src/services/request.js
@@ -1,8 +1,18 @@
 const TOKEN_API = 'https://opentdb.com/api_token.php?command=request';
 const five = 5;
+let category = '';
+
+export async function apiCategory() {
+  const categoryApi = await fetch('https://opentdb.com/api_category.php');
+  const categoryJson = await categoryApi.json();
+  console.log('categoryJson', categoryJson)
+
+  const { trivia_categories } = categoryJson;
+  return trivia_categories;
+}
 
 export async function apiQuestions(token, numberQuestions = five) {
-  const requestApiQuestions = await fetch(`https://opentdb.com/api.php?amount=${numberQuestions}&token=${token}`);
+  const requestApiQuestions = await fetch(`https://opentdb.com/api.php?amount=${numberQuestions}&token=${token}&category=${category}`);
   const requestJsonQuestions = await requestApiQuestions.json();
 
   return requestJsonQuestions;
@@ -13,6 +23,7 @@ export async function apiToken() {
   const requestJsonToken = await requestApiToken.json();
 
   const { token } = requestJsonToken;
+  console.log(token)
   const questions = await apiQuestions(token);
 
   return { token, questions };
